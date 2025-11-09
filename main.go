048e6bc3aa4f4ab5981625a4746f3e733301887f
eisvice <kirillukhanovo@gmail.com>
2025-11-09 23:43:12 +0500

    added validateChirpHandler

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 const port = "8080"
 const filepathRoot = "."
 
 func main() {
+	apiCfg := apiConfig{fileserverHits: atomic.Int32{}}
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", healthHandler)
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.hitsHandler)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	fs := http.FileServer(http.Dir(filepathRoot))
-	mux.Handle("/app/", http.StripPrefix("/app", fs))
+	handler := http.StripPrefix("/app", fs)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 
 	server := &http.Server{
 		Addr: ":" + port,
@@ -28,10 +48,77 @@ func main() {
 func healthHandler(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	writer.WriteHeader(http.StatusOK)
-	num, err := writer.Write([]byte("OK"))
+	_, err := writer.Write([]byte("OK"))
 	if err != nil {
 		log.Println(err)
 	}
-	log.Printf("Some number: %d", num)
-	log.Println(request)
+}
+
+func (cfg *apiConfig) hitsHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	_, err := fmt.Fprintf(writer, `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
+	if err != nil {
+		log.Fatal("hitsHandler issues: ", err)
+	}
+}
+
+func (cfg *apiConfig) resetHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+}
+
+func validateChirpHandler(writer http.ResponseWriter, request *http.Request) {
+	defer request.Body.Close()
+
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+
+	type responseBody struct {
+		Valid bool `json:"valid"`
+	}
+
+	dat, err := io.ReadAll(request.Body)
+	if err != nil {
+		respondWithError(writer, 500, "couldn't read request!")
+		return
+	}
+
+	params := requestBody{}
+	err = json.Unmarshal(dat, &params)
+	if err != nil {
+		respondWithError(writer, 500, "couldn't unmarshal parameters")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(writer, 400, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(writer, 200, responseBody{
+		Valid: true,
+	})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) error {
+    response, err := json.Marshal(payload)
+    if err != nil {
+        return err
+    }
+    w.Header().Set("Content-Type", "application/json")
+    w.Header().Set("Access-Control-Allow-Origin", "*")
+    w.WriteHeader(code)
+    w.Write(response)
+    return nil
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) error {
+	return respondWithJSON(w, code, map[string]string{"error": msg})
 }
